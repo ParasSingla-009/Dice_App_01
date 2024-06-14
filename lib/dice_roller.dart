0e64670aa9e827e35f483c0570c9db015f1f1e22
ParasSingla-009 <chutku202@gmail.com>
2024-06-14 23:51:58 +0530

    first learning flutter project

---
+++ lib/dice_roller.dart
@@ -0,0 +1,42 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/images/dice-6.png';
+
+  void rollDice() {
+    var diceroll = Random().nextInt(6) + 1;
+    setState(() {
+      activeDiceImage = 'assets/images/dice-$diceroll.png';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDiceImage,
+          width: 200,
+        ),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(top: 20),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 28)),
+          child: const Text("Roll Dice"),
+        )
+      ],
+    );
+  }
+}
